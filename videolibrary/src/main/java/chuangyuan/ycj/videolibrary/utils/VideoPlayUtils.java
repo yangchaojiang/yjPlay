2948e2f8f96c95d1511a10adce6495f97fe9718c
yangc <www.1007181167@qq.com>
2017-03-04 18:27:14 +0800

    增加encryption

---
+++ videolibrary/src/main/java/chuangyuan/ycj/videolibrary/utils/VideoPlayUtils.java
@@ -0,0 +1,90 @@
+package chuangyuan.ycj.videolibrary.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.TrafficStats;
+
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.source.BehindLiveWindowException;
+
+/**
+ * Created by yangc on 2017/2/25.
+ * E-Mail:1007181167@qq.com
+ * Description：
+ */
+public class VideoPlayUtils {
+    public static final String TAG = "VideoPlayUtils";
+
+    /***
+     * 获取地当前网速
+     *
+     * @param activity 活动对象
+     * @return long
+     **/
+    public static long getTotalRxBytes(Activity activity) {
+        if (activity == null) return 0;
+        return TrafficStats.getUidRxBytes(activity.getApplicationInfo().uid) == TrafficStats.UNSUPPORTED ? 0 : (TrafficStats.getTotalRxBytes() / 1024);//转为KB
+    }
+
+    /****
+     * kb 转换mb
+     *
+     * @param k 该参数表示kb的值
+     * @return  double
+     */
+    public static double getM(long k) {
+        double m;
+        m = k / 1024.0;
+        return m; //返回kb转换之后的M值
+    }
+
+    /**
+     * 检查当前网络是否可用
+     *@param activity  活动
+     * @return boolean
+     */
+    public static boolean isNetworkAvailable(Activity activity) {
+        if (activity == null) return false;
+        Context context = activity.getApplicationContext();
+        // 获取手机所有连接管理对象（包括对wi-fi,net等连接的管理）
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager == null) {
+            return false;
+        } else {
+            // 获取NetworkInfo对象
+            NetworkInfo[] networkInfo = connectivityManager.getAllNetworkInfo();
+            if (networkInfo != null && networkInfo.length > 0) {
+                for (int i = 0; i < networkInfo.length; i++) {
+                    // 判断当前网络状态是否为连接状态
+                    if (networkInfo[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    /***
+     *
+     *是否TYPE_SOURCE 异常
+     * @param e  异常
+     * @return  boolean
+     * ***/
+    public static boolean isBehindLiveWindow(ExoPlaybackException e) {
+        if (e.type != ExoPlaybackException.TYPE_SOURCE) {
+            return false;
+        }
+        Throwable cause = e.getSourceException();
+        while (cause != null) {
+            if (cause instanceof BehindLiveWindowException) {
+                return true;
+            }
+            cause = cause.getCause();
+        }
+        return false;
+    }
+
+}
---
+++ videolibrary/src/main/java/chuangyuan/ycj/videolibrary/widget/ParamsCreator.java
@@ -0,0 +1,92 @@
+package chuangyuan.ycj.videolibrary.widget;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+public class ParamsCreator {
+	private Context context;
+    private int screenWidth;//屏幕宽度
+    private int screenHeight;//屏幕高度
+    private int densityDpi;//像素密度
+    public ParamsCreator(Context context){
+    	this.context = context;
+    	WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+    	screenWidth = wm.getDefaultDisplay().getWidth();
+    	screenHeight = wm.getDefaultDisplay().getHeight();
+    	DisplayMetrics metric = new DisplayMetrics();
+    	wm.getDefaultDisplay().getMetrics(metric);
+    	densityDpi = metric.densityDpi;
+    }
+    
+	/**
+	 * 获得默认圆的半径
+	 * @return int
+	 */
+	public int getDefaultCircleRadius(){
+    	if(screenWidth >= 1400){//1440
+    		return 50;
+    	}
+    	if(screenWidth >= 1000){//1080
+    		if(densityDpi >=480)
+        		return 48;
+        	if(densityDpi >= 320)
+        		return 48;
+        	return 48;
+    	}
+    	if(screenWidth >= 700){//720
+        	if(densityDpi >= 320)
+        		return 34;
+        	if(densityDpi >= 240)
+        		return 34;
+        	if(densityDpi >= 160)
+        		return 34;
+        	return 34;
+    	}
+    	if(screenWidth >= 500){//540
+        	if(densityDpi >= 320)
+        		return 30;
+        	if(densityDpi >= 240)
+        		return 30;
+        	if(densityDpi >= 160)
+        		return 30;
+        	return 30;
+    	}
+    	return 30;
+	}
+	/**
+	 * 获得默认圆的间距‘
+	 * @return int
+	 */
+	public int getDefaultCircleSpacing(){
+    	if(screenWidth >= 1400){//1440
+    		return 12;
+    	}
+    	if(screenWidth >= 1000){//1080
+    		if(densityDpi >=480)
+        		return 12;
+        	if(densityDpi >= 320)
+        		return 12;
+        	return 12;
+    	}
+    	if(screenWidth >= 700){//720
+        	if(densityDpi >= 320)
+        		return 8;
+        	if(densityDpi >= 240)
+        		return 8;
+        	if(densityDpi >= 160)
+        		return 8;
+        	return 8;
+    	}
+    	if(screenWidth >= 500){//540
+        	if(densityDpi >= 320)
+        		return 5;
+        	if(densityDpi >= 240)
+        		return 5;
+        	if(densityDpi >= 160)
+        		return 5;
+        	return 5;
+    	}
+    	return 5;
+	}
+}
