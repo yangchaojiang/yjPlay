6459540fb0a768a1c74f80a4d69d44e6f22d7d75
“yangchaojianggit config --global user.email www.1007181167@qq.com <www.1007181167@qq.com>
2017-09-11 15:26:38 +0800

    修改

---
+++ vitamioRecorderLibrary/src/main/java/com/yixia/CommonIntentExtra.java
@@ -0,0 +1,25 @@
+package com.yixia;
+
+public class CommonIntentExtra {
+
+	/** 是否退出 */
+	public final static String USER_IS_LOGOUT = "is_logout";
+
+	/** 录制时间控制 */
+	public final static String RECORD_TIME_INTENT = "RECORD_TIME";
+
+	/** 录制信息存储 */
+	public final static String EXTRA_MEDIA_OBJECT = "extra_media_object";
+
+	/** 是否图片导入 */
+	public final static String EXTRA_MEDIA_IMPORT_IMAGE = "extra_media_import_image";
+
+	/** 是否视频导入 */
+	public final static String EXTRA_MEDIA_IMPORT_VIDEO = "extra_media_import_video";
+
+	/** 是否在线视频导入 */
+	public final static String EXTRA_MEDIA_IMPORT_VIDEO_ONLINE = "extra_media_import_video_online";
+
+	/** 多格视频 */
+	public final static String EXTRA_MEDIA_VIDEO_MULTI = "extra_media_video_multi";
+}
