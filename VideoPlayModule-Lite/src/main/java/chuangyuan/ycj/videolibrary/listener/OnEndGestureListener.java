49e527a4854268646db27edda9283bc1216585b3
yangjiang <www.1007181167@qq.com>
2018-11-14 09:11:57 +0800

    提交缺少文件

---
+++ VideoPlayModule-Lite/src/main/java/chuangyuan/ycj/videolibrary/listener/OnEndGestureListener.java
@@ -0,0 +1,9 @@
+package chuangyuan.ycj.videolibrary.listener;
+
+import android.view.MotionEvent;
+
+public interface OnEndGestureListener {
+    void onEndGesture();
+
+    void onTouchEvent(MotionEvent event);
+}
